916f6cea2872bf4f1c8388e67cdd458d2baa8597
Nika <nikaonime@gmail.com>
2023-07-24 10:20:35 +0300

    end of writing to file event(fs, writeFile)

--- 01-event-loop-events/index.js
+++ 01-event-loop-events/index.js
@@ -1,8 +1,12 @@
+const fs = require('fs')
+
 console.log('program start')
 
 setTimeout(() => console.log('timeout 1'), 0)
 setTimeout(() => console.log('timeout 2'), 10)
 
 Promise.resolve().then(() => console.log('promise 1'))
+
+fs.writeFile('./test.txt', 'hello world', () => console.log('file written'))
 
 console.log('program end')
