6e7e2dbd7683e536effae3b72ebe4992fe686eca
Nika <nikaonime@gmail.com>
2023-07-24 11:06:25 +0300

    process.nextTick(), setImmediate()

--- 01-event-loop-events/index.js
+++ 01-event-loop-events/index.js
@@ -1,12 +1,20 @@
 const fs = require('fs')
 
+function timestamp () {
+    return performance.now().toFixed(2)
+}
+
 console.log('program start')
 
-setTimeout(() => console.log('timeout 1'), 0)
-setTimeout(() => console.log('timeout 2'), 10)
+setTimeout(() => console.log('timeout 1', timestamp()), 0)
+setTimeout(() => console.log('timeout 2', timestamp()), 10)
 
-Promise.resolve().then(() => console.log('promise 1'))
+fs.writeFile('./test.txt', 'hello world', () => console.log('file written', timestamp()))
 
-fs.writeFile('./test.txt', 'hello world', () => console.log('file written'))
+Promise.resolve().then(() => console.log('promise 1', timestamp()))
+
+process.nextTick(() => console.log('next tick 1', timestamp()))
+
+setImmediate(()=>console.log('immediate 1', timestamp()))
 
 console.log('program end')
