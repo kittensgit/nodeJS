e8b96f11d2f488bad8c5315a55ab1618460bbe5a
Nika <nikaonime@gmail.com>
2023-07-25 11:26:16 +0300

    fib function without blocking event loop

---
+++ 02-blocking-event-loop/fibonacci-out of-memory.js
@@ -0,0 +1,17 @@
+//0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+
+setTimeout(() => console.log('Timeout'), 0)
+
+function fib(n) {
+    return new Promise((resolve, reject) => {
+        if (n === 0 || n === 1) {
+            return resolve(n)
+        }
+        setImmediate(() =>
+            Promise.all([fib(n - 1), fib(n - 2)])
+                .then(([fib1, fib2]) => resolve(fib1 + fib2))
+        )
+    })
+}
+
+fib(11).then((res) => console.log(res))
