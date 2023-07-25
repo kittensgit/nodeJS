5c6d67a665e77cb26038b16609aa1faea36d958e
Nika <nikaonime@gmail.com>
2023-07-25 13:06:57 +0300

    finding fibonacci numbers without recursion

---
+++ 02-blocking-event-loop/fibonacci-linear-complexity.js
@@ -0,0 +1,28 @@
+//0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+
+function info(text) {
+    console.log(text, performance.now().toFixed(2))
+}
+
+info('program start')
+
+setTimeout(() => info('timeout'), 0)
+
+function fib(n) {
+    if (n === 0 || n === 1) {
+        return n
+    }
+    let fib1 = 0
+    let fib2 = 1
+    let sum
+    for (let i = 1; i < n; i++) {
+        sum = fib1 + fib2
+        fib1 = fib2
+        fib2 = sum
+    }
+    return sum
+}
+
+console.log(fib(100))
+
+info('program end')
