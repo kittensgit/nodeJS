e0fceb971eebb025d684ad7c4aa5ff5b5a109ffb
Nika <nikaonime@gmail.com>
2023-07-26 12:09:24 +0300

    single exports

--- 03-commonjs-modules/index.js
+++ 03-commonjs-modules/index.js
@@ -1,5 +1,10 @@
 const { myName, myHobbies, myFavoriteNumber } = require('./multiple-exports');
+const greetingFn = require('./single-exports');
+//DON'T USE ABSOLUTE PATHS
+// const greetingFn = require('c:/Programming/nodeJS/03-commonjs-modules/single-exports.js');
 
 console.log(myName);
 console.log(myHobbies);
 console.log(myFavoriteNumber);
+
+greetingFn(myName);
---
+++ 03-commonjs-modules/single-exports.js
@@ -0,0 +1,7 @@
+function greeting(name) {
+    console.log('hello', name);
+}
+
+// console.log(__filename);
+
+module.exports = greeting;
