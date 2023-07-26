65bad021a1315d08c81f03d3e9e1932e910dc86f
Nika <nikaonime@gmail.com>
2023-07-26 10:40:25 +0300

    commonJS

---
+++ 03-commonjs-modules/index.js
@@ -0,0 +1,11 @@
+// console.log(arguments.callee.toString());
+
+// console.log(module);
+
+// console.log(__filename);
+
+// console.log(__dirname);
+
+// console.log(exports === module.exports);
+
+// console.log(require);
